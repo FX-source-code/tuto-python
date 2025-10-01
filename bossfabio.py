b885c048c3e1645a77dc66361133053907c01aed
FX <fxminecraft23@gmail.com>
2025-10-01 10:34:43 +0200

    coded

---
+++ bossfabio.py
@@ -0,0 +1,64 @@
+# --------------- 1er cours ----------------
+# = commentaire 
+# print c'est une fonction pour afficher dans la console 
+# console = ce que je vois ( résultat finale )
+# () = l'argument est à l'imtérieur des parenthèses
+# Ex: print("bonjour")
+# Une variable : ex: bonjour = 1 
+#
+# Une Variable : ex: A = 1 
+# 
+# Pour utiliser des variables dans des arguments : print(A) et comme on a mis avant A=1 cela va afficher 1
+#
+# pour créer du texte : texte = "asdfghjkl"
+#il faut prendre en compte que les expaces n'existent pas : c'est un : _ 
+# 1er exercice : affiche-moi bonjour puis crée une variable qui s'applle pomme et doit être égale à 3+2 puis affiche la variable dans la console
+
+print("bonjour")
+
+pomme = 3+2
+
+print(pomme)
+
+# quand on affiche une variable dansla console il faut mettre : et pomme = 3+2 print(pomme) 
+
+
+x = wouou!
+
+print(x)
+print(type(x))
+
+# type () c pour dire quel type de varible il s'agit de : str--> pour les textes , int il s'agit des chiffres entiers
+# float --> pour tous les nombres ex 1,512309840239876984948276912
+# exercices : créer 2 variables : un variable pomme et l'autre bananes , la variable pomme=5 et la variable banane=8 , autre variable qui s'appelle prix totale (le prix des 2 variable)
+
+# exercies 
+
+
+pomme = 5 
+banane = 8 
+prix total = pomme + banane 
+
+print(prix_total)
+
+#opération sur les nombres 
+
+# additioner : +
+# soustraire : - 
+# multiplier : * 
+# puissance : ** 
+# division : / 
+# division sans reste : // 
+# pour otenir le reste : % 
+
+
+
+
+
+#-------------------2ème cours-------------------------
+
+# Compter les caractères dans les chaînes & conditions et les boucles 
+# Input 
+
+
+# Un input sert à demander à l'utilisateur du text 
